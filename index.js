ee6a6984861acd9078b739e91b63ccf2d31a66f6
Pedro Reis <contatodopedroreis@gmail.com>
2021-02-18 19:06:34 -0300

    fim

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/**
- * @format
- */
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
 
-import { AppRegistry } from "react-native";
-import App from "./src/App";
-import { name as appName } from "./app.json";
+require('react-native').unstable_enableLogBox();
+import { AppRegistry } from 'react-native';
+import App from './src';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
